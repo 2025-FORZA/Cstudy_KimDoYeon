10e9cdc6f4ef8a4b13ef96c0e5d0b805abd2e8fc
dayfuti546 <dayfuti546@sookmyung.ac.kr>
2025-04-02 09:44:06 +0900

    1_lab02

---
+++ 1_lab02_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    long long A, B, C;
+
+    // 사용자 입력 받기
+    scanf("%lld %lld %lld", &A, &B, &C);
+
+    // 합 계산 및 출력
+    printf("%lld\n", A + B + C);
+
+    return 0;
+}
